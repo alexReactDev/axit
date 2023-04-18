e5db31ff9594392ba2f966be0a29ed1a1b6d030b
Alexander <alexander.work.mailbox@gmail.com>
2023-04-18 19:51:56 +0600

    Added header

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 "use strict"
+window.addEventListener("load", () => {
+	const burger = document.querySelector(".burger");
+	burger.onclick = openMenu;
+})
+
+function openMenu() {
+	const burger = document.querySelector(".burger");
+	const menu = document.querySelector(".header__nav");
+	burger.classList.add("burger_active");
+	menu.classList.add("header__nav_active");
+	burger.onclick = closeMenu;
+}
+
+function closeMenu() {
+	const burger = document.querySelector(".burger");
+	const menu = document.querySelector(".header__nav");
+	burger.classList.remove("burger_active");
+	menu.classList.remove("header__nav_active");
+	burger.onclick = openMenu;
+}
